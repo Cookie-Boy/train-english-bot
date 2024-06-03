66992db0f7b046d75b3be1cd25e745aa1dcc805c
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2024-06-03 19:18:13 +0700

    Create echo-bot

---
+++ src/main/java/org/myproject/train_english_bot/config/TelegramBotConfiguration.java
@@ -0,0 +1,32 @@
+package org.myproject.train_english_bot.config;
+
+import lombok.SneakyThrows;
+import org.myproject.train_english_bot.service.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SuppressWarnings("unused")
+@Configuration
+public class TelegramBotConfiguration {
+    @Bean
+    public TelegramBot telegramBot(
+            @Value("${bot.token}") String token,
+            @Value("${bot.name}") String name,
+            TelegramBotsApi telegramBotsApi
+    ) throws TelegramApiException {
+        var bot = new TelegramBot(new DefaultBotOptions(), token, name);
+        telegramBotsApi.registerBot(bot);
+        return bot;
+    }
+
+    @Bean
+    @SneakyThrows
+    public TelegramBotsApi telegramBotsApi() {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
